404fddaa004be2caba0ee268422fd305cafdfd6d
Anton Dobrohorskyi <zore3x@gmail.com>
2018-07-16 09:58:35 +0300

    create MainController.java to call methods to retrieve data from the database

---
+++ src/main/java/artoscats/MainController.java
@@ -0,0 +1,33 @@
+package artoscats;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller()
+@RequestMapping(path="/catsApi")
+public class MainController {
+
+    @Autowired
+    private CatRepository catRepository;
+
+    @PutMapping(path="/insert") // Map ONLY GET Requests
+    public @ResponseBody
+    String insertCat (@RequestParam String name
+            , @RequestParam Integer age) {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+
+        Cat cat = new Cat();
+        cat.setName(name);
+        cat.setAge(age);
+        catRepository.save(cat);
+        return "Inserted";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<Cat> getAllCats() {
+        // This returns a JSON or XML with the users
+        return catRepository.findAll();
+    }
+}
